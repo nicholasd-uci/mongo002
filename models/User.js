e87da767bdaa894e488a683c9ca8cb1283d956f2
Nicholas Dallas <nicholas.dallas0@gmail.com>
2020-09-10 14:08:11 -0700

    updated QF notes

--- models/User.js
+++ models/User.js
@@ -3,26 +3,29 @@ const { model, Schema } = require('mongoose')
 // schema is the overall layout of the data //
 // model is the the collection //
 
+const { model, Schema } = require('mongoose')
+
 const User = new Schema({
-    name: {
-        type: String,
-        unique: true,
-        required: true
-    }, 
-    username: {
-        type: String,
-        unique: true,
-        required: true
-    },
-    email: {
-        type: String,
-        unique: true,
-        required: true
-    },
-    items: [{
-        type: Schema.Types.ObjectId,
-        ref: 'Item'
-    }]
+  name: {
+    type: String,
+    required: true
+  },
+  username: {
+    type: String,
+    unique: true,
+    required: true
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true
+  },
+  items: [{
+    type: Schema.Types.ObjectId,
+    ref: 'Item'
+  }]
 }, { timestamps: true })
+
+User.plugin(require('passport-local-mongoose'))
 
 module.exports = model('User', User)
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,34 +1,55 @@
 const router = require('express').Router()
 const { User } = require('../models')
+const passport = require('passport')
+const jwt = require('jsonwebtoken')
 
-
-//   GET all users(read)
-router.get('/user', (req, res) => {
-    User.find()
-        .populate('items')
-        .then(users => res.json(users))
-        .catch(err => console.log(err))
+router.post('/users/register', (req, res) => {
+  const { name, email, username, password } = req.body
+  User.register(new User({ name, email, username }), password, err => {
+    if (err) { console.log(err) }
+    res.sendStatus(200)
+  })
 })
 
-//  POST one user(create)
-router.post('/user', (req, res) => {
-    User.create(req.body)
-        .then(user => res.json(user))
-        .catch(err => console.log(err))
+router.post('/users/login', (req, res) => {
+  const { username, password } = req.body
+  User.authenticate()(username, password, (err, user) => {
+    if (err) { console.log(err) }
+    res.json(user ? jwt.sign({ id: user._id }, process.env.SECRET) : null)
+  })
 })
 
-//   PUT one user (Update)
-router.put('/users/:id', (req, res) => {
-    User.findByIdAndUpdate(req.params.id, req.body)
-    .then(() => res.sendStatus(200))
-    .catch(err => console.log(err))
+router.get('/users/post', passport.authenticate('jwt'), (req, res) => {
+    res.send
 })
 
-//  DELETE one user 
-router.delete('/users/:id', (req, res) => {
-    User.findByIdAndDelete(req.params.id)
-    .then(() => res.sendStatus(200))
-    .catch(err => console.log(err))
-})
+// GET all users
+// router.get('/users', (req, res) => {
+//   User.find()
+//     .populate('items')
+//     .then(users => res.json(users))
+//     .catch(err => console.log(err))
+// })
+
+// POST one user
+// router.post('/users', (req, res) => {
+//   User.create(req.body)
+//     .then(user => res.json(user))
+//     .catch(err => console.log(err))
+// })
+
+// PUT one user
+// router.put('/users/:id', (req, res) => {
+//   User.findByIdAndUpdate(req.params.id, req.body)
+//     .then(() => res.sendStatus(200))
+//     .catch(err => console.log(err))
+// })
+
+// DELETE one user
+// router.delete('/users/:id', (req, res) => {
+//   User.findByIdAndDelete(req.params.id)
+//     .then(() => res.sendStatus(200))
+//     .catch(err => console.log(err))
+// })
 
 module.exports = router
