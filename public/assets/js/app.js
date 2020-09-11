5c49384f29e35a78a466e8065dfe8f7bbe6ca222
Nicholas Dallas <nicholas.dallas0@gmail.com>
2020-09-11 11:51:58 -0700

    udpated

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,53 +1,53 @@
 axios.get('/api/users/items', {
-    headers: {
-      Authorization: `Bearer ${localStorage.getItem('user')}`
-    }
+  headers: {
+    Authorization: `Bearer ${localStorage.getItem('user')}`
+  }
+})
+  .then(({ data: user }) => {
+    document.getElementById('username').textContent = `Welcome ${user.username}!`
+
+    user.items.forEach(item => {
+      let itemElem = document.createElement('li')
+      itemElem.className = item.isDone ? 'list-group-item list-group-item-success' : 'list-group-item'
+      itemElem.textContent = item.text
+      document.getElementById('items').append(itemElem)
+    })
   })
-    .then(({ data: user }) => {
-      document.getElementById('username').textContent = `Welcome ${user.username}!`
-  
-      user.items.forEach(item => {
+  .catch(err => {
+    console.error(err)
+    window.location = '/auth.html'
+  })
+
+  document.getElementById('addItem').addEventListener('click', event => {
+    event.preventDefault()
+
+    axios.post('/api/items', {
+      text: document.getElementById('item').value,
+      isDone: false
+    }, {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem('user')}`
+      }
+    })
+      .then(({ data: item }) => {
         let itemElem = document.createElement('li')
         itemElem.className = item.isDone ? 'list-group-item list-group-item-success' : 'list-group-item'
         itemElem.textContent = item.text
         document.getElementById('items').append(itemElem)
+        document.getElementById('item').value = ''
       })
-    })
-    .catch(err => {
-      console.error(err)
-      window.location = '/auth.html'
-    })
-  
-    document.getElementById('addItem').addEventListener('click', event => {
-      event.preventDefault()
-  
-      axios.post('/api/items', {
-        text: document.getElementById('item').value,
-        isDone: false
-      }, {
-        headers: {
-          Authorization: `Bearer ${localStorage.getItem('user')}`
-        }
+      .catch(err => {
+        console.error(err)
+        Toastify({
+          text: 'Woops! Something went wrong. Try again.',
+          backgroundColor: 'red',
+          duration: 3000
+        }).showToast()
       })
-        .then(({ data: item }) => {
-          let itemElem = document.createElement('li')
-          itemElem.className = item.isDone ? 'list-group-item list-group-item-success' : 'list-group-item'
-          itemElem.textContent = item.text
-          document.getElementById('items').append(itemElem)
-          document.getElementById('item').value = ''
-        })
-        .catch(err => {
-          console.error(err)
-          Toastify({
-            text: 'Woops! Something went wrong. Try again.',
-            backgroundColor: 'red',
-            duration: 3000
-          }).showToast()
-        })
-    })
+  })
+
+  document.getElementById('signOut').addEventListener('click', event => {
+    localStorage.removeItem('user')
+    window.location = '/auth.html'
+  })
   
-    document.getElementById('signOut').addEventListener('click', event => {
-      localStorage.removeItem('user')
-      window.location = '/auth.html'
-    })
-    
